79168a4c4f8ac095a620c0cd47a2652d37df78a8
EzeaniChucks <concord_chucks2@yahoo.com>
2024-10-19 08:12:47 +0100

    Improved texts on UI

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -7,11 +7,16 @@ const Intro = () => {
       <h2 className="flex gap-4 text-white text-4xl">
         Hi there! <PiHandWaving className="text-orange-500" />
       </h2>
-      <h3 className="text-white max-w-[500px] text-xl md:text-3xl font-extralight">
-        {
-          "I'm Ezeani Chukwudi, an experienced FullStack mobile and web developer. I have created real-world solutions relied upon by thousands of users for their daily activities."
-        }
-      </h3>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-white max-w-[500px] text-xl md:text-3xl font-extralight">
+          {"I'm Ezeani Chukwudi, a FullStack (mobile and web) developer."}
+        </h3>
+        <h4 className="text-white max-w-[500px] text-sm md:text-md font-extralight">
+          {
+            "I have created real-world solutions relied upon by thousands of users for their daily activities."
+          }
+        </h4>
+      </div>
       <a href="mailto:concord_chucks2@yahoo.com">
         <button className="flex items-center gap-4 p-3 px-5 rounded-3xl bg-white text-black hover:text-white hover:bg-black hover:border border-white">
           Get in Touch <BsArrowRightCircle />{" "}
