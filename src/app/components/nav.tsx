9d19dd3cb7d6b9f1d2646494269259cdceb67c8d
EzeaniChucks <concord_chucks2@yahoo.com>
2024-10-19 07:53:49 +0100

    updated my cv

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <div className="flex justify-between items-center h-20 w-full">
       <h2 className="text-white text-4xl font-bold">EC</h2>
-      <a href="/Ezeani_Chucks_CV.pdf" download={true}>
+      <a href="/CHUCKS_CV_OnePage.pdf" download={true}>
         <button className="flex items-center gap-4 p-3 px-5 rounded-3xl bg-white text-black hover:text-white hover:bg-black hover:border border-white">
           Download CV <BsArrowDownCircle />{" "}
         </button>
